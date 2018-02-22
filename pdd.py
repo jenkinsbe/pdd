3e1587542604e0e3b2112b4c59a8ae7fb7614224
Ben Jenkins <ben.jenkins789@gmail.com>
2018-02-22 13:51:32 +1100

    upload

--- pdd.py
+++ pdd.py
@@ -3,26 +3,72 @@
 #import gtk
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk as gtk
+from gi.repository import Gtk, GObject
+import time
+from threading import Thread
 
-class Application:
 
-  def on_window1_destroy(self, object, data=None):
-    print ("quit with cancel")
-    gtk.main_quit()
+class InterFace(Gtk.Window):
 
-  def on_gtk_quit_activate(self, menuitem, data=None):
-    print ("quit from menu")
-    gtk.main_quit()
+    def on_window_destroy(self, object, data=None):
+        print ("Quiting")
+        Gtk.main_quit()
+    
+    def btnNextJob(self, object, data=None):
+        print ("Next job")
 
-  def __init__(self):
-    self.gladefile = "pdd_main.glade"
-    self.builder = gtk.Builder()
-    self.builder.add_from_file(self.gladefile)
-    self.builder.connect_signals(self)
-    self.window = self.builder.get_object("window1")
-    self.window.show()
+    def btnPreviousJob(self, object, data=None):
+        print ("Previous job")
+        
+    def airfield_changed(self, object, data=None):
+        print ("Airfield changed")
+        
+        text = object.get_active_text()
+        if text is not None:
+            print ("Airfield selected is: %s" % text)
+
+
+    def counting_monkeys(self):
+        # replace this with your thread to update the text
+        n = 1
+        while True:
+            time.sleep(1)
+            newtext = str(n)+" monkey" if n == 1 else str(n)+" monkeys"
+            GObject.idle_add(
+                self.tbPagerMessage.set_text, newtext,
+                priority=GObject.PRIORITY_DEFAULT
+                )
+            n += 1
+            
+    def __init__(self):
+        self.gladefile = "pdd_main.glade"
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(self.gladefile)
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("mainwindow")
+        self.window.show()
+
+        self.tbPagerMessage = self.builder.get_object("tbPagerMessage")
+        self.tbWeather = self.builder.get_object("tbWeather")
+        self.tbFlightPath = self.builder.get_object("tbFlightPath")
+        self.tbClosestAirbase = self.builder.get_object("tbClosestAirbase")
+
+        # 1. define the tread, updating your text
+        self.update = Thread(target=self.counting_monkeys)
+        # 2. Deamonize the thread to make it stop with the GUI
+        self.update.setDaemon(True)
+        # 3. Start the thread
+        self.update.start()
+
+  
+
+  
+def run_gui():
+    main = InterFace()
+    # 4. this is where we call GObject.threads_init()
+    GObject.threads_init()
+    main.show_all()
+    Gtk.main()
 
 if __name__ == "__main__":
-  main = Application()
-  gtk.main()
+    run_gui()  
